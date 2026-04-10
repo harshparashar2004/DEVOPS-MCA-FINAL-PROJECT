3d04272381213fee5c434b3b818e9352442fcc1f
unknown <harshparashar11012004@gmail.com>
2026-04-10 10:05:25 +0530

    New version

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+// Serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
-  res.send("DevOps Project Running 🚀");
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
+  console.log("Open your browser at http://localhost:3000");
 });
